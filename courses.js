11ddaf4b6db6fab97cb07172fb49b435c2db18de
matildaharaldsson00 <97900355+matildaharaldsson00@users.noreply.github.com>
2022-03-28 20:31:15 +0200

    nya funktioner, fungerar dock inte som det ska

--- courses.js
+++ courses.js
@@ -1,71 +1,94 @@
 "use strict"; 
 
 // global variabel för sökrutan
-let inputCourse = document.getElementById("courses");
+// let inputCourse = document.getElementById("courses");
 
-//global variabel för att komma åt lärarna i databasen
+//globala variabel för att komma åt studenterna, lärarna och kurserna i databasen
 let allTeachers = DATABASE.teachers;
+let allStudents = DATABASE.students;
+let allCourses = DATABASE.courses;
 
 // funktion för att rensa sökrutan när man uppdaterar sidan
-window.onload = function() {
-    document.getElementById("courses").value = "";
-};
+// window.onload = function() {
+//     document.getElementById("courses").value = "";
+// };
 
 // funktion för att hitta kurserna i databasen baserat på kurs id
-function findCoursesByTitle() {
-    let course = DATABASE.courses.filter((course) => 
-    course.title.toLowerCase().includes(inputCourse.value.toLowerCase())
-    );
-    return course;
-}
+// function findCoursesByTitle() {
+//     let course = DATABASE.courses.filter((course) => 
+//     course.title.toLowerCase().includes(inputCourse.value.toLowerCase())
+//     );
+//     return course;
+// }
 
-// funktion för att skapa kursen med information
-function renderCourse(course) {
-    let divCourse = document.createElement("div");
+// 
+// function renderCourse(course) {
+//     let divCourse = document.createElement("div");
 
-    let courseDiv = document.getElementById("searchCourseResult");
-    courseDiv.appendChild(divCourse);
+//     let courseDiv = document.getElementById("searchCourseResult");
+//     courseDiv.appendChild(divCourse);
 
-    divCourse.classList.add("course")
+//     divCourse.classList.add("course")
     
-    divCourse.innerHTML = `
-    <div>${course.title} Totalt ${course.totalCredits} högksolepoäng</div>
-    <div id=teacher>Lärare:</div>
-    `;
-}
-
-function allTeacherInfo(id) {
-    let course = DATABASE.courses[i];
-    let teachersNames = allTeachers.map((teacher) => teacher.firstName + " " + teacher.lastName + " " + `(${teacher.post})`);
-    let teachers = [];
-
-    for(let i = 0; i < teachersNames.length; i++) {
-        if(course.teachers.some((value) => value == i)) {
-            let div = document.createElement("div");
-            let info = div.innerHTML = `<p>${teachersNames[i]}</p>`
-        }
-    }
-    return teachers.toString().split(",").join(" ");
-}
+//     divCourse.innerHTML = `
+//     <div>${course.title} Totalt ${course.totalCredits} högksolepoäng</div>
+//     <div id=teacher>Lärare: ${allTeacherInfo}</div>
+//     <div id=students>Studenter som gått kursen: ${allStudentInfo}</div>
+//     `;
+// }
 
 // funktion som ska gå igenom varje kurs och lägger till HTML
+// function renderCourses(courses) {
+//     courses.forEach((course) => {
+//         renderCourse(course);
+//     });
+// }
+function renderCourse(id) {
+    let div = document.createElement("div");
+    div.id = "course-div";
+    div.innerHTML += `
+    <h2>${allCourses.title} (Totalt ${allCourses.totalCredits} högskolepoäng)</h2>
+    <h3>Lärare:</h3>
+    <div id=teachers-div>
+    ${allTeacherInfo(id)}
+    </div>
+    <h3>Studenter:</h3>
+    <div id=course-div>
+    ${allStudentInfo(id)}
+    </div>
+    `;
+    return div;
+}
+
+// funktion för att hitta alla kurserna
 function renderCourses(courses) {
-    courses.forEach((course) => {
-        renderCourse(course);
-    });
+    let coursesElement = document.getElementById("searchCourseResult");
+
+    for(let course of courses) {
+        let courseElement = renderCourse(course.courseId);
+        coursesElement.appendChild(courseElement);
+    }
+}
+
+function passedCredits(student) {
+    let credits = [];
+    for(let course of student.courses) {
+        credits.push(course.passedCredits);
+    }
+    return credits;
 }
 
 // funktion som hittar kursansvarig
 function courseResponsible(id) {
-    let course = DATABASE.courses[i];
+    let course = DATABASE.courses[id];
     let teachersNames = allTeachers.map((teacher) => teacher.firstName + " " + teacher.lastName + " " + `(${teacher.post})`);
-    let res = course.courseResponsible;
-    return teachersNames[res];
+    let resp = course.courseResponsible;
+    return teachersNames[resp];
 }
 
 // info om lärarna
 function allTeacherInfo(id) {
-    let course = DATABASE.courses[i];
+    let course = DATABASE.courses[id];
     let teachersNames = allTeachers.map((teacher) => teacher.firstName + " " + teacher.lastName + " " + `(${teacher.post})`);
     let teachers = [];
 
@@ -79,46 +102,47 @@ function allTeacherInfo(id) {
     return teachers.toString().split(",").join(" ");
 }
 
-// student information
-
-// funktion för att hitta lärare 
-function allCourseTheachers(id) {
-    let course = DATABASE.courses[i];
-    let teachersNames = allTeachers.map((teacher) => teacher.firstName + " " + teacher.lastName + " " + `(${teacher.post})`);
-    let teachers = [];
-    for(let i = 0; i < teachers.length; i++) {
-        if(course.teachers.some((value) => value == i)){
-            let div = document.createElement("div");
-            let content = div.innerHTML = `<p>${teachersNames[i]}</p>`
-            teachers.push(content);
-        }
-    }
-    return teachers.toString().split(".").join(" ");
-}
-
-//funktion för att hitta studenterna för varje kurs
-function allCourseStudents(id) {
-    let courseId = DATABASE.courses[i].courseId;
+// information om studenterna
+function allStudentInfo(id) {
+    let courseId = DATABASE.courses[id].courseId;
     let students = [];
-    for(let student of allstudents) {
-        console.log(courses.courseId, courseId)
-            if(courses.courseId == courseId) {
+
+    for(let student of allStudents) {
+        for(let course of student.courses) {
+            if(course.courseId == courseId) {
                 students.push(student);
             }
+        }
     }
+    return students.toString().split(",").join("");
 }
 
-
-
 // event-listener för att kära funktionerna och filtrera genom kurserna varje gång sök trycks
-inputCourse.addEventListener("keyup", function() {
-    let course = findCoursesByTitle();
-    let coursesDiv = document.getElementById("searchCourseResult");
+// inputCourse.addEventListener("keyup", function() {
+//     let course = renderCourses();
+//     let coursesDiv = document.getElementById("searchCourseResult");
 
-    coursesDiv.innerHTML = "";
-    renderCourses(course);
+//     coursesDiv.innerHTML = "";
+//     renderCourses(course);
 
-    if(inputCourse.value == 0) {
-        coursesDiv.innerHTML = "";
+//     if(inputCourse.value == 0) {
+//         coursesDiv.innerHTML = "";
+//     }
+// });
+
+function inputResult() {
+    let arrayResult = [];
+    let input = document.getElementById("courses");
+
+    for(let i = 0; i < allCourses.length; i++) {
+        document.querySelector("#searchCourseResult").innerHTML = "";
+        if("" == input.value) {
+            document.querySelector("#searchCourseResult").innerHTML = "";
+        } else if (allCourses[i].title.toLowerCase().includes(input.value.toLowerCase())) {
+            arrayResult.push(allCourses[i]);
+        }
     }
-});
+    renderCourses(arrayResult);
+}
+
+document.getElementById("courses").addEventListener("keyup", inputResult);
